acc674e504337521e70968e4d2facb6f5cedc036
ilian <heatblazer@gmail.com>
2025-03-19 22:20:22 +0200

    refactor

--- src/main/java/com/example/demo/RestClientConfig.java
+++ src/main/java/com/example/demo/RestClientConfig.java
@@ -7,6 +7,8 @@ import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import io.micrometer.common.lang.Nullable;
+
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
@@ -36,16 +38,18 @@ public class RestClientConfig {
 	private static final Logger LOGGER = Logger.getLogger("RestClientConfig.java");
 
     @Bean
-    public RestTemplate restTemplate() throws KeyManagementException {
+    @Nullable
+    public RestTemplate restTemplate()  {
         SSLContext sslContext = configureSSLContext();
         return new RestTemplate(createRequestFactory(sslContext));
     }
 
-    private SSLContext configureSSLContext() throws KeyManagementException {
-        try {
+    private SSLContext configureSSLContext()  {
+    	SSLContext sslContext = null;
+    	try {
         	LOGGER.info("STARTING CONFIG!!!");
         	
-            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext = SSLContext.getInstance("TLS");
 
             KeyStore keyStore = KeyStore.getInstance("PKCS12");
             char[] keyStorePassword = pkcs12Password.toCharArray();
@@ -65,13 +69,16 @@ public class RestClientConfig {
 
             TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
             trustManagerFactory.init(trustStore);
-
+            try {
             sslContext.init(keyManagerFactory.getKeyManagers(), null/*trustManagerFactory.getTrustManagers()*/, null);
+            } catch (Exception ex) {
+            	return sslContext;
+            }            
             LOGGER.info("END CONFIG");
             return sslContext;
         } catch (NoSuchAlgorithmException | KeyStoreException | CertificateException | IOException e) {
-            throw new RuntimeException("Error configuring SSLContext", e);
-        }
+        	return sslContext;
+        } 
     }
 
     private ClientHttpRequestFactory createRequestFactory(SSLContext sslContext) {
